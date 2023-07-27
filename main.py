9efe800dddcfa06bf2403cdbf626db5e6eafa07c
Anastasiia Nifantova <anastasiianifantova@Anastasiias-MacBook-Pro.local>
2023-07-28 00:00:49 +0200

    pirson works

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pandas as pnd
 import matplotlib.pyplot as plt
+from correlation_coefficient import correlation_coefficient
 import math
 import sys
-
-import numpy as np  #for train
 
 theta0 = 0
 theta1 = 0
@@ -13,8 +12,12 @@ theta1 = 0
 # Понимание гипотезы для прогнозирования цены машины по пробегу
 # (estimatePrice(mileage) = θ0 + (θ1 * mileage)).
 
-def correlation_coefficient_test() -> bool:
-    # pirson = 
+def correlation_coefficient_test(data_csv) -> bool:
+    mileage = [data_row['km'] for (index, data_row) in data_csv.iterrows()]
+    prices = [data_row['price'] for (index, data_row) in data_csv.iterrows()]
+    pirson_coefficient = correlation_coefficient(mileage, prices)
+
+    print("Коэффициент корреляции Пирсона:", pirson_coefficient)
     return True
 
 
@@ -23,7 +26,7 @@ def correlation_coefficient_test() -> bool:
 
 def load_data(path):
     data_csv = pnd.read_csv(path) #to_scv -- writer
-    print(data_csv)
+    # print(data_csv)
     return data_csv
 
 
@@ -41,12 +44,12 @@ def build_graph(data_csv):
     plt.title('Distribution of car prices relative to mileage')
     plt.rcParams.update({'font.size': 14})
 
-    if theta0 != 0 and theta1 != 0:
-        x = np.linspace(0, 1, 100)
-        f = 0.25 - (x - 0.5)**2
-        plt.plot(x, f, '--r')
-        plt.plot(x, f, '--r', label=f'f(x) = {theta1} * x + {theta0}')
-        plt.legend(fontsize=12)
+    # if theta0 != 0 and theta1 != 0:
+    #     x = np.linspace(0, 1, 100)
+    #     f = 0.25 - (x - 0.5)**2
+    #     plt.plot(x, f, '--r')
+    #     plt.plot(x, f, '--r', label=f'f(x) = {theta1} * x + {theta0}')
+    #     plt.legend(fontsize=12)
 
 
     plt.show()
@@ -75,7 +78,9 @@ def cost_forecast(milage) -> int:
 
 def main():
     data = load_data("data.csv")
+    correlation_coefficient_test(data)
     build_graph(data)
+
 
 # if __name__ == "__main__":
 main()
