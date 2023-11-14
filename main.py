8308da110472de80df494a01cd79c9a70b61e968
Anastasiia Nifantova <anastasiianifantova@Anastasiias-MacBook-Pro.local>
2023-11-14 23:39:10 +0100

    plotly added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import pandas as pnd
+import pandas as pd
 import matplotlib.pyplot as plt
 from correlation_coefficient import correlation_coefficient
+import plotly.express as px
 import math
 import sys
 
@@ -25,8 +26,8 @@ def correlation_coefficient_test(data_csv) -> bool:
 # Подготовка и предобработка данных для обучения модели.
 
 def load_data(path):
-    data_csv = pnd.read_csv(path) #to_scv -- writer
-    # print(data_csv)
+    data_csv = pd.read_csv(path) #to_scv -- writer
+    # print(data_csv.head())
     return data_csv
 
 
@@ -38,21 +39,51 @@ def build_graph(data_csv):
     mileage = [data_row['km'] for (index, data_row) in data_csv.iterrows()]
     prices = [data_row['price'] for (index, data_row) in data_csv.iterrows()]
 
-    plt.plot(mileage, prices, 'ob')
-    plt.xlabel('Mileage')
-    plt.ylabel('Price')
-    plt.title('Distribution of car prices relative to mileage')
-    plt.rcParams.update({'font.size': 14})
+    # Создаем DataFrame из данных
+    data = {'Mileage': mileage, 'Price': prices}
+    df = pd.DataFrame(data)
 
+    # Строим точечный график
+    fig = px.scatter(df, x='Mileage', y='Price', title='Distribution of car prices relative to mileage')
+
+    # Настройка стиля графика
+    fig.update_traces(marker=dict(color='blue', size=8))
+
+    # Строим линейную регрессию, если theta0 и theta1 не равны 0
     if theta0 != 0 and theta1 != 0:
-        x = range(int(min(mileage)), int(max(mileage)) + 1)
-        f = [theta1 * xi + theta0 for xi in x]
-        plt.plot(x, f, '--r')
-        plt.plot(x, f, '--r', label=f'f(x) = {theta1} * x + {theta0}')
-        plt.legend(fontsize=12)
+        x_range = range(int(min(mileage)), int(max(mileage)) + 1)
+        y_range = theta1 * x_range + theta0
+        fig.add_trace(px.line(x=x_range, y=y_range, mode='lines', line=dict(color='red', dash='dash'), name=f'f(x) = {theta1} * x + {theta0}').data[0])
+
+    # Настройка осей и шрифта
+    fig.update_xaxes(title_text='Mileage')
+    fig.update_yaxes(title_text='Price')
+    fig.update_layout(font=dict(size=14))
+
+    # Настройка размера графика
+    fig.update_layout(height=500, width=700)
+
+    # где то тут добавить легенду
+
+    # Показываем график
+    fig.show()
+
+    # то же самое в матлолибе
+    # plt.plot(mileage, prices, 'ob')
+    # plt.xlabel('Mileage')
+    # plt.ylabel('Price')
+    # plt.title('Distribution of car prices relative to mileage')
+    # plt.rcParams.update({'font.size': 14})
+
+    # if theta0 != 0 and theta1 != 0:
+    #     x = range(int(min(mileage)), int(max(mileage)) + 1)
+    #     f = [theta1 * xi + theta0 for xi in x]
+    #     plt.plot(x, f, '--r')
+    #     plt.plot(x, f, '--r', label=f'f(x) = {theta1} * x + {theta0}')
+    #     plt.legend(fontsize=12)
 
 
-    plt.show()
+    # plt.show()
 
 
 def train_model(data_csv, learning_rate, num_iterations):
@@ -88,6 +119,7 @@ def cost_forecast(milage) -> int:
 #TODO 7 Визуализация результатов (бонусная часть)
 # Построение графика данных и линии регрессии, полученной после обучения модели.
 # Добавление легенды и названий осей для лучшей визуализации.
+
 
 def main():
     data = load_data("data.csv")
